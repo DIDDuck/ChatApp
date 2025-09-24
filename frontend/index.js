bec39c57344b2254b27d63ee99dfdfdc63983f9d
DIDDuck <179592401+DIDDuck@users.noreply.github.com>
2025-09-24 17:34:09 +0300

    Added markdown to html formatting using showdown.js library. In streaming mode received text is first brought visible in plain text and finally after receiving full message conversed to html.

--- frontend/index.js
+++ frontend/index.js
@@ -1,7 +1,8 @@
-import { local, backendUrl } from "./configuration.js";
+import { local, backendUrl } from "./configuration.js"; 
 
 const errorElement = document.getElementById("error-field");
 const messagesDiv = document.getElementById("messages-div");
+const converter = new showdown.Converter(); // Markdown to html converter
 
 if (messagesDiv.querySelectorAll("div").length === 0 && !messagesDiv.classList.contains("hidden")) {
     messagesDiv.classList.add("hidden");
@@ -13,7 +14,7 @@ const send = async (event) => {
     // Get form values
     const userMessageText = document.getElementById("user-message-box").value;
     const streamCheckbox = document.getElementById("stream-checkbox");
-    if (local) console.log("QUESTION:", userMessageText);
+    if (local) console.log("USER MESSAGE:", userMessageText);
 
     // Insert user message to conversation box
     messagesDiv.appendChild(createMessageDiv("User:", userMessageText, messagesDiv));
@@ -71,18 +72,26 @@ const send = async (event) => {
                         } else { // the rest of parts
                             currentMessage = "{" + messages[i] + "false}";
                         }
-                        newMessageDiv.querySelectorAll("p")[1].innerText += JSON.parse(currentMessage)["response"];
+                        newMessageDiv.querySelector("div").innerText += JSON.parse(currentMessage)["response"];
                     }
                     if (local) console.log(`SPLIT ERROR MESSAGE: ${index}`)
                 }
-                else newMessageDiv.querySelectorAll("p")[1].innerText += JSON.parse(message)["response"];
+                else newMessageDiv.querySelector("div").innerText += JSON.parse(message)["response"];
                 index++;
             }
+            // When text is complete, convert from markdown to html
+            const completeText = newMessageDiv.querySelector("div").innerText;
+            newMessageDiv.querySelector("div").innerText = "";
+            newMessageDiv.querySelector("div").innerHTML = converter.makeHtml(completeText);
+            applyColorTheme();
+
         // Read response in one piece
         } else {
             const data = await response.json();
             if (local) console.log("DATA: ", data);
+            
             messagesDiv.appendChild(createMessageDiv("AI Assistant:", data.text, messagesDiv));
+            applyColorTheme();
         } 
 
     } catch (error) {
@@ -97,29 +106,56 @@ const send = async (event) => {
 const createMessageDiv = (role, text, parent ) => {
     const messageDiv = document.createElement("div");
     
-    // If already some messages in parent, add margin
+    // If already some messages in parent, add margin and if dark theme apply it
     if (parent.querySelectorAll("div").length !== 0){
-        messageDiv.classList.add("mt-10");
+        messageDiv.classList.add("mt-20");
     }
+    // Are we in dark mode? 
+    if (document.body.classList.contains("dark")) {
+            messageDiv.classList.add("dark");
+        }
     
     const messageRole = document.createElement("p");
-    messageRole.classList.add("bold");
+    messageRole.classList.add("bold", "mb-10");
     messageRole.innerText = role;
-    const messageText = document.createElement("p");
-    messageText.classList.add("mx-10")
-    messageText.innerText = text;
+    const messageText = document.createElement("div");
+    messageText.classList.add("mx-10", "pre")
+    messageText.innerHTML = converter.makeHtml(text);
     messageDiv.appendChild(messageRole);
     messageDiv.appendChild(messageText);
 
     return messageDiv;
 };
 
-const darkMode = () => {
+const changeColorTheme = () => {
     document.body.classList.toggle("dark");
     document.getElementById("user-message-box").classList.toggle("dark");
     document.getElementById("messages-div").classList.toggle("dark");
     document.getElementsByClassName("container")[0].classList.toggle("dark");
+    document.querySelectorAll("#messages-div>div").forEach(element => {
+        element.classList.toggle("dark")
+    });
+    document.querySelectorAll("#messages-div code").forEach(element => {
+        element.classList.toggle("dark")
+    });
+};
+
+
+const applyColorTheme = () => {
+    const darkMode = document.body.classList.contains("dark") ? true : false
+    // Some elements that we want different classes have been created after theme change, code blocks for example
+    if (darkMode) {
+        document.querySelectorAll("#messages-div code").forEach(element => {
+        const isDark =  element.classList.contains("dark");
+        if (!isDark) element.classList.add("dark");
+        });
+    } else {
+        document.querySelectorAll("#messages-div code").forEach(element => {
+        const isDark =  element.classList.contains("dark");
+        if (isDark) element.classList.remove("dark");
+        });
+    }
 };
 
 window.send = send;
-window.darkMode = darkMode;
+window.changeColorTheme = changeColorTheme;
